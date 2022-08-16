ea780455eac0733e549ef2e770dc5ad16240d313
Carlos Akión Garro Campos <carloscamp1008@estudianTEC.cr>
2022-08-16 00:23:46 -0600

    Revert "Update"
    This reverts commit 34811776eb91269a16c20f6d570b2656ed84ed1d.

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './view/dashboard/dashboard.component';
 import { LoginComponent } from './view/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -1,18 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {
-  GoogleLoginProvider,
-  SocialAuthService,
-  SocialAuthServiceConfig,
-  SocialLoginModule,
-} from 'angularx-social-login';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './templates/header/header.component';
 import { FooterComponent } from './templates/footer/footer.component';
+
 import { LoginComponent } from './view/login/login.component';
-import { DashboardComponent } from './view/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -20,25 +14,9 @@ import { DashboardComponent } from './view/dashboard/dashboard.component';
     HeaderComponent,
     FooterComponent,
     LoginComponent,
-    DashboardComponent,
   ],
-  imports: [BrowserModule, SocialLoginModule, AppRoutingModule],
-  providers: [
-    {
-      provide: 'SocialAuthServiceConfig',
-      useValue: {
-        autoLogin: false,
-        providers: [
-          {
-            id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider(
-              '452056001459-g9jqelh9s9le7e0er3m57nramcq5to7r.apps.googleusercontent.com'
-            ),
-          },
-        ],
-      } as SocialAuthServiceConfig,
-    },
-  ],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
