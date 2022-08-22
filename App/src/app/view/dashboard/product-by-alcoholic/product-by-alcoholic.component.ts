ea7ae188839fc6a51c894932c77311de53455c11
Carlos Akión Garro Campos <carloscamp1008@estudianTEC.cr>
2022-08-21 22:31:03 -0600

    Working Alcoholic and Glasses

--- App/src/app/view/dashboard/product-by-alcoholic/product-by-alcoholic.component.ts
+++ App/src/app/view/dashboard/product-by-alcoholic/product-by-alcoholic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnectionService } from 'src/app/services/api/connection.service';
 
 @Component({
   selector: 'app-product-by-alcoholic',
@@ -6,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-by-alcoholic.component.css'],
 })
 export class ProductByAlcoholicComponent implements OnInit {
-  constructor() {}
+  products = Array<any>();
+  categories = Array<any>();
+  selectedValue: any;
+  changeCategoryProducts(e) {
+    this.selectedValue = e.target.value;
+  }
 
-  ngOnInit(): void {}
+  constructor(private api: ConnectionService) {}
+
+  ngOnInit(): void {
+    this.api.getAlcoholicFilters().subscribe((data) => {
+      this.categories = data['drinks'];
+      console.log(this.categories);
+    });
+
+    this.api.getNonOrAlcoholicCocktail('Alcoholic').subscribe((data) => {
+      this.products = data['drinks'];
+    });
+  }
+
+  newProduct() {
+    this.api.getNonOrAlcoholicCocktail(this.selectedValue).subscribe((data) => {
+      this.products = data['drinks'];
+    });
+  }
 }
--- App/src/app/view/dashboard/product-by-glass/product-by-glass.component.ts
+++ App/src/app/view/dashboard/product-by-glass/product-by-glass.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnectionService } from 'src/app/services/api/connection.service';
 
 @Component({
   selector: 'app-product-by-glass',
@@ -6,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-by-glass.component.css'],
 })
 export class ProductByGlassComponent implements OnInit {
-  constructor() {}
+  products = Array<any>();
+  categories = Array<any>();
+  selectedValue: any;
+  changeCategoryProducts(e) {
+    this.selectedValue = e.target.value;
+  }
 
-  ngOnInit(): void {}
+  constructor(private api: ConnectionService) {}
+
+  ngOnInit(): void {
+    this.api.getGlasses().subscribe((data) => {
+      this.categories = data['drinks'];
+    });
+
+    this.api.getCocktailByGlass('Cocktail_glass').subscribe((data) => {
+      this.products = data['drinks'];
+    });
+  }
+
+  newProduct() {
+    this.api.getCocktailByGlass(this.selectedValue).subscribe((data) => {
+      this.products = data['drinks'];
+    });
+  }
 }
